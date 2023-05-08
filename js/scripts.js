cc828511c66978c4b23f91eab46fcdca6c62ebe5
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-05-08 08:37:09 -0400

    Added greeting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+alert('Hello World');
+
+let favoriteFood = 'Beef Wellington';
+document.write(favoriteFood);
