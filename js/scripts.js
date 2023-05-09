215235b3b78d68d4f3765ea83af14a31479cd6a2
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-05-09 11:35:23 -0400

    Added initial Pokemon to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-let pokemonList = {
-    name: '',
-    height: '',
-    types: [],
-};
+let pokemonList = [
+    {
+    name: 'Charizard',
+    height: 1.7 /'m',
+    types: ['Fire', 'Flying'],
+    },
+    {
+    name: 'Alakazam',
+    height: 1.5 /'m',
+    types: ['Psychic'],
+    },
+    {
+    name: 'Onix',
+    height: 8.8 /'m',
+    types: ['Rock', 'Ground'],
+    }
+]
