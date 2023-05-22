148e65ede156dda757c446c3fa2998a382606c5b
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-05-22 17:13:12 -0400

    added modal template for the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const pokemonRepository = (function () {
   const pokemonList = [];
   const apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  //modalContainer Variable to select the id from the html for the modal makeup
+  let modalContainer = document.querySelector('#modal-container'); 
 
 //Function adds pokemon and validates typeof
 function add(pokemon) {
@@ -74,6 +76,57 @@ function loadDetails(item) {
     console.error(e);
   });
 } //Function to load details pulled from detailsurl in the api
+
+//Show Modal Function
+function showModal(title, text) {//paramaters to enable modal in ()
+  // Clear all existing modal content
+  modalContainer.innerHTML = '';
+  //Creates the Modal as a Div element in html with the class called modal
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+  // Add the new modal content, Close button element created named button assigned to model close element in HTML
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');//CSS For modal close state
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);//executes hide modal when clicked
+  //Create title of modal
+  let titleElement = document.createElement('h1');
+  titleElement.innerText = title;
+  //Create inner content of modal
+  let contentElement = document.createElement('p');
+  contentElement.innerText = text;
+  //Adds elements to modal and modal to container
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modalContainer.appendChild(modal);
+  //Adds is-visible styling from css
+  modalContainer.classList.add('is-visible'); 
+}
+//Hide modal function
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+//Hide via escape key
+window.addEventListener('keydown', (e) => {
+  let modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();//If key press is the escape key this function will call the hide modal function  
+  }
+});
+//close on click outside of modal
+modalContainer.addEventListener('click', (e) => {
+  // Since this is also triggered when clicking INSIDE the modal
+  // We only want to close if the user clicks directly on the overlay
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
+
+document.querySelector('#show-modal').addEventListener('click', () => {
+  showModal('pokemon', 'pokemon height');
+});
 
 //Return Functions
 return {
